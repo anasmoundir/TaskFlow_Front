9b47373c62d30bc1f13464d8886e703d76606277
anasmoundir <anasmoundir1996@gmail.com>
2024-01-08 09:25:09 +0100

    structureOfTheProject

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TaskDetailComponent } from './components/task-detail/task-detail.component';
+import { TaskListComponent } from './components/task-list/task-list.component';
+import { UserListComponent } from './components/user-list/user-list.component';
+import { TokenListComponent } from './components/token-list/token-list.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path:"task-details", component : TaskDetailComponent},
+  {path:"task-list",component:TaskListComponent},
+  {path:"user-list",component:UserListComponent},
+  {path:"token-list",component:TokenListComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TaskListComponent } from './components/task-list/task-list.component';
+import { TaskDetailComponent } from './components/task-detail/task-detail.component';
+import { TokenListComponent } from './components/token-list/token-list.component';
+import { UserListComponent } from './components/user-list/user-list.component';
+import { StoreModule } from '@ngrx/store';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { EffectsModule } from '@ngrx/effects';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TaskListComponent,
+    TaskDetailComponent,
+    TokenListComponent,
+    UserListComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StoreModule.forRoot({}),
+    StoreModule.forRoot({}, {}),
+    EffectsModule.forRoot([])
   ],
   providers: [],
   bootstrap: [AppComponent]
